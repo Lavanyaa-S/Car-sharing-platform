bcc75df54795d491d41f6d76b49daa055cd39455
Lavanyaa S <109542568+Lavanyaa-S@users.noreply.github.com>
2023-03-08 16:25:57 +0000

    Updated the home page with important components

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+servicetypes1 = [{
+  'id': 1,
+  'Service_type': "Become a host",
+  'Description': "Share your car with us and earn!!!",
+  'Activity': "Share my car"
+}, {
+  'id': 2,
+  'Service_type': "Rent a car",
+  'Description': "Rent a car and have a happy journey!!",
+  'Activity': "Rent a car"
+}]
+
 
 @app.route("/")
 def homepage():
-  return render_template("home.html")
+  return render_template("home.html", servicetypes=servicetypes1)
+
+
 if __name__ == "__main__":
-  app.run('0.0.0.0',debug=True)
-  
+  app.run('0.0.0.0', debug=True)
