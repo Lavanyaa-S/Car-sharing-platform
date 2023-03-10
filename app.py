c30637584b1ab9e2ba375c1d4d3583fb1f26f06e
Lavanyaa S <109542568+Lavanyaa-S@users.noreply.github.com>
2023-03-10 18:17:55 +0000

    Added responsive pages for the host to share their data

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from database import load_cardata_from_db, add_application_to_db
 
 app = Flask(__name__)
 servicetypes1 = [{
   'id': 1,
   'Service_type': "Become a host",
   'Description': "Share your car with us and earn!!!",
-  'Activity': "Share my car"
+  'Activity': "Share my car",
+  'link': "Host"
 }, {
   'id': 2,
   'Service_type': "Rent a car",
   'Description': "Rent a car and have a happy journey!!",
-  'Activity': "Rent a car"
+  'Activity': "Rent a car",
+  'link': "User"
 }]
 
 
 @app.route("/")
 def homepage():
   return render_template("home.html", servicetypes=servicetypes1)
+
+
+@app.route("/Host")
+def gethostdetails():
+  return render_template("hostdetail.html")
+
+
+@app.route("/User")
+def getcarlist():
+  return render_template("carslist.html")
+
+
+@app.route("/host/apply", methods=['post'])
+def getdata():
+  data = request.form
+  add_application_to_db(data)
+  #return jsonify(data)
+  return render_template('app_submitted.html',application=data)
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy import *
+import mysql.connector
+
+db_connection_string = "mysql+pymysql://kkvtffkdk6kggcx53xwe:pscale_pw_ko5zYg2BkIQWYDPavNm3ccE73NR9kYc7UHTX3YL9L7J@ap-south.connect.psdb.cloud/carsharing?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_cardata_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from carhostdet"))
+    result_dicts = []
+    for row in result.all():
+      result_dicts.append(row._asdict())
+  print(result_dicts)
+
+
+def add_application_to_db(data):
+  mydb = mysql.connector.connect(
+  host="ap-south.connect.psdb.cloud",
+  user="kkvtffkdk6kggcx53xwe", password="pscale_pw_ko5zYg2BkIQWYDPavNm3ccE73NR9kYc7UHTX3YL9L7J", database="carsharing")
+  mycursor = mydb.cursor()
+
+  sql = "INSERT INTO carhostdet (hostname,pickuppoint,hostmail,hphne,cbrand,cmodel,startdate,enddate) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)"
+  val = (data["hostname"],data["hplace"],data["mail"],data["phno"],data["cbrand"],data["cmodel"],data["stdate"],data["endate"])
+  mycursor.execute(sql, val)
+
+  mydb.commit()
+    
