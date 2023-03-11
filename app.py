04c3b59eeb37cff97fee199751fff3bfd0b40822
Lavanyaa S <109542568+Lavanyaa-S@users.noreply.github.com>
2023-03-11 10:54:25 +0000

    A fully functional test data contained website: Does not have any original information.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-from database import load_cardata_from_db, add_application_to_db
+from flask import Flask, render_template, request
+from database import load_cardata_from_db, add_application_to_db, listpossiblecars,returning_dict_of_possible_cars
+import re, datetime
 
 app = Flask(__name__)
 servicetypes1 = [{
@@ -26,18 +27,88 @@ def homepage():
 def gethostdetails():
   return render_template("hostdetail.html")
 
+@app.route("/contactus")
+def getcontactdetails():
+  return render_template("contact.html")
+
 
 @app.route("/User")
 def getcarlist():
   return render_template("userdata.html")
 
+@app.route("/Booked")
+def succesfull():
+  return render_template("booked.html")
+
+
+@app.route("/user/rent", methods=['post'])
+def putdata():
+  reqdata = request.form
+  place = reqdata['uplace']
+  idlist = load_cardata_from_db(place)
+
+  sdate1 = reqdata['stdate']
+  edate1 = reqdata['endate']
+
+  days1 = re.search('\d{4}-\d{2}-\d{2}', sdate1)
+  reqdatestart = datetime.datetime.strptime(days1.group(), '%Y-%m-%d')
+  days2 = re.search('\d{4}-\d{2}-\d{2}', edate1)
+  reqdateend = datetime.datetime.strptime(days2.group(), '%Y-%m-%d')
+  rstart = reqdatestart.date()
+  rend = reqdateend.date()
+
+  finalidlist = []
+
+  for ids in idlist:
+    id = listpossiblecars(ids, rstart, rend)
+    if id > 0:
+      daysrented=(rend-rstart).days
+      finalidlist.append(id)
+  if len(finalidlist) == 0:
+    return render_template("carslist.html")
+  else:
+    listofdictofdata=returning_dict_of_possible_cars(finalidlist,daysrented)
+    return render_template("listingcars.html",listofdictofdata=listofdictofdata)
+
+ 
 
 @app.route("/host/apply", methods=['post'])
 def getdata():
+  amount = {
+    "Punto": 1800,
+    "Grande punto": 2140,
+    "Avventura": 1680,
+    "Ecosport": 1600,
+    "Everest": 1250,
+    "Ranger": 1570,
+    "Amaze": 1600,
+    "City": 1580,
+    "Jazz": 1650,
+    "Ace": 1200,
+    "Nano": 1000,
+    "ALTROZ": 1780,
+    "Rapid style": 1400,
+    "Rapid Elegance": 2310
+  }
   data = request.form
   add_application_to_db(data)
-  #return jsonify(data)
-  return render_template('app_submitted.html', application=data)
+
+  sdate = data['stdate']
+  edate = data['endate']
+
+  day = re.search('\d{4}-\d{2}-\d{2}', sdate)
+  startdate = datetime.datetime.strptime(day.group(), '%Y-%m-%d').date()
+  day2 = re.search('\d{4}-\d{2}-\d{2}', edate)
+  enddate = datetime.datetime.strptime(day2.group(), '%Y-%m-%d').date()
+
+  carmodel = data['cmodel']
+  dayscalc = (enddate - startdate).days
+  calcamnt = dayscalc * amount[carmodel]
+  
+  return render_template('app_submitted.html',
+                         application=data,
+                         amount=calcamnt,
+                         dayscalc=dayscalc)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy import *
+import re, datetime
 import mysql.connector
 
 db_connection_string = "mysql+pymysql://kkvtffkdk6kggcx53xwe:pscale_pw_ko5zYg2BkIQWYDPavNm3ccE73NR9kYc7UHTX3YL9L7J@ap-south.connect.psdb.cloud/carsharing?charset=utf8mb4"
@@ -10,27 +11,109 @@ engine = create_engine(db_connection_string,
                        }})
 
 
-def load_cardata_from_db():
+def returning_dict_of_possible_cars(idlist,days):
   with engine.connect() as conn:
-    result = conn.execute(text("select * from carhostdet"))
+    amount = {
+    "Punto": 1800,
+    "Grande punto": 2140,
+    "Avventura": 1680,
+    "Ecosport": 1600,
+    "Everest": 1250,
+    "Ranger": 1570,
+    "Amaze": 1600,
+    "City": 1580,
+    "Jazz": 1650,
+    "Ace": 1200,
+    "Nano": 1000,
+    "ALTROZ": 1780,
+    "Rapid style": 1400,
+    "Rapid Elegance": 2310
+    }
+
+    result = conn.execute(text("select * from carhostdet "))
+    result_dicts = []
+    for row in result.all():
+      temp=row._asdict()
+      if temp['id'] in idlist:
+        model=temp['cmodel']
+        amnt=days*amount[model]
+        temp2={"amount":amnt}
+        temp.update(temp2)
+        result_dicts.append(temp)
+  return result_dicts
+
+
+
+def load_cardata_from_db(place):
+  with engine.connect() as conn:
+    result = conn.execute(text("select id,pickuppoint from carhostdet "))
+    idlist=[]
     result_dicts = []
     for row in result.all():
       result_dicts.append(row._asdict())
     for i in result_dicts:
-      date1=i['startdate']
-      date2=i['enddate']
-  print(result_dicts)
+      if i["pickuppoint"]==place:
+        idlist.append(i['id'])
+  return idlist
 
+
+
+def listpossiblecars(id,reqdatestart,reqdateend):
+  mydb = mysql.connector.connect(host="ap-south.connect.psdb.cloud",user="kkvtffkdk6kggcx53xwe",password="pscale_pw_ko5zYg2BkIQWYDPavNm3ccE73NR9kYc7UHTX3YL9L7J",database="carsharing")
+  mycursor = mydb.cursor()
+
+  sql = "SELECT * FROM carhostdet WHERE id = %s"
+  adr = (id, )
+
+  mycursor.execute(sql, adr)
+
+  amount = {
+    "Punto": 1800,
+    "Grande punto": 2140,
+    "Avventura": 1680,
+    "Ecosport": 1600,
+    "Everest": 1250,
+    "Ranger": 1570,
+    "Amaze": 1600,
+    "City": 1580,
+    "Jazz": 1650,
+    "Ace": 1200,
+    "Nano": 1000,
+    "ALTROZ": 1780,
+    "Rapid style": 1400,
+    "Rapid Elegance": 2310
+  }
+
+  myresult = mycursor.fetchall()
+  if len(myresult)==0:
+    return -9
+
+  for x in myresult:
+    savaildate=x[7]
+    eavaildate=x[8]
+    if reqdatestart<reqdateend:
+      if savaildate<=reqdatestart<=eavaildate:
+        if savaildate<=reqdateend<=eavaildate:
+          return x[0]
+        else:
+          return -9
+      else:
+        return -9
+    else:
+      return -9
+    
 
 def add_application_to_db(data):
   mydb = mysql.connector.connect(
-  host="ap-south.connect.psdb.cloud",
-  user="kkvtffkdk6kggcx53xwe", password="pscale_pw_ko5zYg2BkIQWYDPavNm3ccE73NR9kYc7UHTX3YL9L7J", database="carsharing")
+    host="ap-south.connect.psdb.cloud",
+    user="kkvtffkdk6kggcx53xwe",
+    password="pscale_pw_ko5zYg2BkIQWYDPavNm3ccE73NR9kYc7UHTX3YL9L7J",
+    database="carsharing")
   mycursor = mydb.cursor()
 
   sql = "INSERT INTO carhostdet (hostname,pickuppoint,hostmail,hphne,cbrand,cmodel,startdate,enddate) VALUES (%s, %s,%s, %s,%s, %s,%s, %s)"
-  val = (data["hostname"],data["hplace"],data["mail"],data["phno"],data["cbrand"],data["cmodel"],data["stdate"],data["endate"])
+  val = (data["hostname"], data["hplace"], data["mail"], data["phno"],
+         data["cbrand"], data["cmodel"], data["stdate"], data["endate"])
   mycursor.execute(sql, val)
 
   mydb.commit()
-    
